2e0f4ff26d366beb4a02e9e525a98d2daf6d42b7
ekkopy <thiagognlcn@gmail.com>
2020-05-25 22:15:55 -0300

    Community and Community Addres - still working on :construction:

---
+++ src/community/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+class CommunityAddress(models.Model):
+    """
+    Endereços dentro de uma comunidade
+    """
+    community_address = models.CharField(
+        max_length=100
+    )
+    community_number = models.CharField(
+        max_length=100
+    )
+    
+    def __str__(self):
+        return self.community_address
+
+class Community(models.Model):
+    """
+    Comunidade
+    """
+    community_name = models.CharField(
+        max_length=50
+    )
+    community_image = models.ImageField(
+        upload_to='img_community'
+    )
+    community_data = models.ForeignKey(
+        CommunityAddress,
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.community_name
+        
+    
